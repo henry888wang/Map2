6dbb371f24e8c9b3d0a615a616f4a09382247bab
henry888wang <65693102+henry888wang@users.noreply.github.com>
2020-06-17 22:38:29 +0800

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+var x=0;
+function blah(){
+
+
+
+
+
+if(x==1){
+div1.style.display='block';
+div1.style.animation='swissland 3s  1';
+div2.style.display='block';
+div2.style.animation='iceland 3s  1';
+div3.style.display='block';
+div3.style.animation='morocco 3s  1';
+div4.style.display='block';
+div4.style.animation='england 3s  1';
+div5.style.display='block';
+div5.style.animation='france 3s  1';
+
+map.style.animation='map 3s 1';
+h1.style.animation='abc 2s 1';
+h1.style.opacity='0%';
+btnx.style.animation='abc 2s 1';
+btnx.style.opacity='0%';
+map.style.opacity='100%';
+h2.style.display='block';
+h2.style.animation='h2 linear 2s 3s 1';
+}else{
+
+    x=1;
+
+    window.open('https://xinmeti.co/doc/4-2212/view','newwindow','height=600,width=800,top=200px,left=600px,toolbar=no,menubar=no,scrollbars=no, resizable=no,location=no, status=no')
+}
+
+}
+
